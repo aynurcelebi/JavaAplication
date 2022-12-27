4045a80c5f70c4849790b947657525c85b5f92c8
aynurcelebi <aynurcelebi8947@gmail.com>
2022-12-28 02:33:54 +0300

    Product class and products attributes added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    Product product = new Product();
+    product.id=1;
+    product.name="Laptop";
+    product.description="ASUS Laptop";
+    product.price= 5.500;
+    product.stockAmount=3;
+
+
+        System.out.println("Ürün Adı: "+ product.name);
+        System.out.println("Ürün Fiytı: "+ product.price);
     }
 }
