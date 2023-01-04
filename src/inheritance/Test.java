55a512fd09041706420a2b5ffcd03db8b6da30f8
aynurcelebi <aynurcelebi8947@gmail.com>
2023-01-05 01:55:33 +0300

    Classes Defined

--- src/inheritance/Test.java
+++ src/inheritance/Test.java
@@ -5,6 +5,8 @@ public class Test
     public static void main(String[] args){
 
         Employee employee = new Employee();
+        Customer customer = new Customer();
+
 
 
     }
---
+++ src/inheritanceDemo/Main.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+// Bir bankanın kredi uygulaması inheritance örneği üzerinden oluşturulacak,
+// amaç; birbirine bağımlı olmayan ve yeniliğe karşı direnmeyen kodlar yazmak..
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
